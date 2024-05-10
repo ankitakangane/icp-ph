f1e76126402717daf3fd0337fb657bb71e33bd03
ankitakangane <kanganeankita1@gmail.com>
2024-05-10 15:00:46 +0530

    file added

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_REQUEST['txtName'];
+$email = $_REQUEST['txtEmail'];
+$con = mysqli_connect("localhost","root","","mydb") or die("Error Connection Database");
+$q = mysqli_query($con,"insert into employees(name,email) values ('$name',$email')");
+if($q){
+    echo "record inserted successfully";
+}
+else{
+    echo mysqli_error($con);
+}
